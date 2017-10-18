3b4094512d50a225a734ad8568d39a1b363b3395
juracer <juracer-cabral1@hotmail.com>
2017-10-17 22:21:30 -0200

    1.2

---
+++ index.php
@@ -0,0 +1,447 @@
+<?php
+//Aqui puxamos a cabeça do documento
+require_once ('./php/header.php');
+
+
+?>
+
+    <body>
+        <div class="pushWrapper">
+            <!-- Header (shown on mobile only) -->
+            <header class="pageHeader">
+                <!-- Menu Trigger -->
+                <button class="menu-trigger">
+                <span class="lines"></span>
+            </button>
+
+                <!-- Logo -->
+                <a class="headerLogo smoothScroll" href="index.html" style="align:center">
+                <i class=" step icon-bank size-24 icon-gold"></i>
+                <span class="text">Maison Chic</span>
+            </a>
+            </header>
+
+            <div class="sidebar">
+
+                <nav class="mainMenu">
+                    <ul class="menu">
+
+                        <li>
+                            <a class="smoothScroll" href="html/books.html" title="Get a home">
+                            <i class="icon-presentation size-24 icon-gold"></i><span class="text">Album de Fotos</span></a>
+                        </li>
+
+                        <li>
+                            <a class="smoothScroll" href="html/contatos.html" title="Contact Form">
+                            <i class="step icon-envelope-1 size-24 icon-gold"></i><span class="text">Contatos</span></a>
+                        </li>
+
+                        <li>
+                            <a class="smoothScroll" href="login.html" title="login">
+                            <i class="icon-settings-wheel size-24 icon-gold"></i><span class="text">Adm</span></a>
+                        </li>
+
+                        <li>
+                            <a class="smoothScroll" href="#eventos" title="eventos">
+                            <i class="icon-group size-24 icon-gold"></i><span class="text">Eventos</span></a>
+                        </li>
+
+                        <li>
+                            <a class="smoothScroll" href="#comentarios" title="Comentarios">
+                            <i class="icon-identification size-24 icon-gold"></i><span class="text">Comentários</span></a>
+                        </li>
+
+                    </ul>
+                </nav>
+
+                <nav class="backToTop">
+                    <ul class="backToTop-menu">
+                        <li>
+                            <a class="smoothScroll" href="#section-intro" title="to the top">
+
+                            <span class="text" style="color:red">Topo</span></a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+            <!-- Share Menu -->
+            <nav class="shareMenu">
+                <a href="#" class="share-menu-trigger">
+                <i class="step icon-thumbs-up size-24 icon-gold"></i>
+            </a>
+
+                <div class="tweet-share">
+
+                    <!--Links Sociais  -->
+                    <a style="position: relative; float: left; top: 0%; left:2%; padding-right: 5px;" href="https://api.whatsapp.com/send?l=pt&phone=55219999999"><img src="images/whatsapp.png" height="50" width="50"></a>
+                    <a style="position: relative; float: left; top: 0%; left:2%;" href="https://www.facebook.com/messages/t/968815286480388"><img src="images/messenger.png" height="50" width="50"></a>
+                    <a style="position: relative; float: left; top: 0%; left:2%;" href="https://www.instagram.com/maison_spa/"><img src="images/instagram.png" height="50" width="50"></a>
+                    <a style="position: relative; float: left; top: 0%; left:2%;" href="https://www.facebook.com/maisonchic.salaodebeleza/"><img src="images/facebook.png" height="50" width="50"></a>
+                    <script>
+                        ! function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0],
+                                p = /^http:/.test(d.location) ? 'http' : 'https';
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = p + '://platform.twitter.com/widgets.js';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, 'script', 'twitter-wjs');
+
+                    </script>
+                    <script>
+                        ! function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0],
+                                p = /^http:/.test(d.location) ? 'http' : 'https';
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = p + '://platform.twitter.com/widgets.js';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, 'script', 'twitter-wjs');
+
+                    </script>
+
+                </div>
+            </nav>
+
+            <!-- Main -->
+            <main>
+
+                <!-- FORMS -->
+
+                <!-- INTRO -->
+
+                <section class="section-intro" id="section-intro" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="0">
+
+                    <div class="content">
+                        <div class="container-template">
+                            <h1>
+                                <i class=" step icon-diamond size-24 icon-gold"></i>
+                                <center><span class="seconday"><img src="images/logo_maison_branco.png" width="50%"></span></center>
+
+                            </h1>
+                        </div>
+                    </div>
+
+                </section>
+                <!-- inicio do site -->
+
+                <br>
+                <section class="stastistical-part" id="stastistical-part">
+
+                    <!-- section-seperator -->
+                    <div class="section-seperator">
+                        <div class="container-template">
+                            <h2>
+                                <span class="seconday">Sessão</span> Jô Rigon</h2>
+                        </div>
+                    </div>
+                </section>
+
+                <!-- SECTION -->
+
+                <section class="section-team" id="section-team">
+
+                </section>
+                <!-- SECTION -->
+                <section class="contact-form">
+
+                    <div class="headergroup"></div>
+
+                    <div class="container"></div>
+                    <!-- End Inner -->
+
+                    <br />
+
+                    <div class="container-template" id="section-team">
+
+                        <div class="headergroup">
+                            <h2 class="in-point">
+                                <span class="seconday text-gold">Você Merece Jô Rigon</span>
+                                <span>Serviços de beleza com requinte</span>
+
+                                <span class="seconday">Maison Chic é o melhor para as pessoas mais exigentes.</span>
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+                <section class="section-team">
+                    <!-- Team Members  -->
+
+                    <div id="team-member" class="container">
+                        <br>
+
+                        <div class="team-member-text content-template col-sm-12">
+
+                            <div class="row" align="center" id="projeto">
+
+
+                                <a href="#revele" data-toggle="collapse">
+                                <img src="images/jo.jpg" class="img-circle person" alt="noiva" width="355" height="355">
+                            </a>
+                                <div id="revele" class="collapse">
+                                    <img src="images/mch5.png" class="img" alt="noiva1" width="1155" height="355">
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </section>
+
+
+                <!-- SALÃO SECTION ---------------------->
+
+                <section class="stastistical-part" id="stastistical-part">
+
+                    <!-- section-seperator -->
+                    <div class="section-seperator">
+                        <div class="container-template">
+                            <h2>
+                                <span class="seconday">Sessão</span> SALÃO
+                            </h2>
+                        </div>
+                    </div>
+
+                </section>
+
+                <section class="stastistical-part" id="stastistical-part">
+
+                </section>
+
+                <section class="timeline-part" id="#">
+
+                    <div class="content">
+                        <div class="container-template">
+
+                            <div class="headergroup">
+                                <h2 class="in-point">
+                                    <span class="seconday text-gold">Tour Maison Chic</span>
+                                    <span> Sempre Bela</span>
+                                </h2>
+                            </div>
+                            <!--   Carossel -->
+
+                            <div class="w3-content w3-display-container">
+                                <img class="mySlides" src="fotos/ft.jpg" style="width:100%">
+                                <img class="mySlides" src="fotos/ft2.jpg" style="width:100%">
+                                <img class="mySlides" src="fotos/ft3.jpg" style="width:100%">
+                                <img class="mySlides" src="fotos/ft4.jpg" style="width:100%">
+
+                                <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+                                <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+                            </div>
+
+                            <script>
+                                var slideIndex = 1;
+                                showDivs(slideIndex);
+
+                                function plusDivs(n) {
+                                    showDivs(slideIndex += n);
+                                }
+
+                                function showDivs(n) {
+                                    var i;
+                                    var x = document.getElementsByClassName("mySlides");
+                                    if (n > x.length) {
+                                        slideIndex = 1
+                                    }
+                                    if (n < 1) {
+                                        slideIndex = x.length
+                                    }
+                                    for (i = 0; i < x.length; i++) {
+                                        x[i].style.display = "none";
+                                    }
+                                    x[slideIndex - 1].style.display = "block";
+                                }
+
+                            </script>
+
+                        </div>
+                    </div>
+
+                </section>
+
+                <!-- PAGE SOCIAL SECTION ------------------------------------>
+
+                <section class="stastistical-part" id="eventos">
+
+                    <!-- section-seperator -->
+                    <div class="section-seperator">
+                        <div class="container-template">
+                            <h2>
+                                <span class="seconday">Sociais</span> EVENTOS
+                            </h2>
+                        </div>
+                    </div>
+
+                    <!--  SECTION-->
+
+                    <section class="events-part" id="events-part">
+
+                    </section>
+
+                    <!-- SECTION-->
+
+                    <section class="section-team" id="section-team">
+
+                    </section>
+                    <!-- SECTION-->
+                    <section class="contact-form" id="contact-form">
+
+                        <div class="headergroup">
+
+                        </div>
+
+                        <div class="container">
+
+                        </div>
+                        <!-- End Inner -->
+
+                        <br />
+
+                    </section>
+
+
+                    <section>
+
+                        <!-- Rede Social -->
+                        <section class="testimonials-part" id="eventos">
+
+                            <div class="container-fluid " style="color:white; opacity: 0.6">
+                                <div class="row-fluid">
+                                    <div class="container-template">
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                    </div>
+                                    <div class="container-template">
+                                        <!--Body content-->
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <!-- rede social -->
+
+                        <br/>
+
+                        <!-- SECTION----------------------------------------------------- -->
+
+                        <section class="stastistical-part" id="comentarios">
+
+                            <!-- section-seperator -->
+                            <div class="section-seperator">
+                                <div class="container-template">
+                                    <h2>
+                                        <span class="seconday">Sociais</span> COMENTÁRIOS
+                                    </h2>
+                                </div>
+                            </div>
+
+                            <!-- Aqui vou inserir o Comentário do facebook -->
+                            <div class="fb-comments" data-href="http://www.maisonchic.com.br" data-numposts="7"></div>
+
+
+                        </section>
+                        <!--  SECTION-->
+
+                        <section class="events-part" id="events-part">
+
+                        </section>
+
+                        <!-- SECTION-->
+
+                        <section class="section-team" id="section-team">
+
+                        </section>
+                        <!-- SECTION-->
+                        <section class="contact-form" id="contact-form">
+
+                            <div class="headergroup">
+
+                            </div>
+
+                            <div class="container">
+
+                            </div>
+                            <!-- End Inner -->
+
+                            <br>
+
+                        </section>
+
+                        <section class="section-team">
+
+
+                            <div class="container-fluid" style="color:white; opacity: 0.6">
+                                <div class="row-fluid">
+                                    <div class="span2">
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                    </div>
+                                    <div class="span10">
+                                        <!--Body content-->
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                        <br/>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+
+                        < <footer class="pageFooter">
+                            <div class="btnContainer">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14709.779214319804!2d-43.0036293!3d-22.8230274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x319a15cd458bfb70!2sMaison+Chic+Sal%C3%A3o+de+Beleza+e+Aluguel+Trajes+de+Noivas!5e0!3m2!1spt-BR!2sbr!4v1508243407316" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+
+                            <div class="btnContainer">
+                                <a class="LC-logo" href="#/">
+                                <i class="icon-LClogo"></i>
+                            </a>
+                            </div>
+                            </footer>
+
+            </main>
+
+        </div>
+        <script type='text/javascript' src='js/jquery.js'></script>
+        <script type='text/javascript' src='js/jquery-migrate.js'></script>
+        <script type='text/javascript' src='js/jquery.form.js'></script>
+        <script type='text/javascript' src='js/jquery.mobile.custom.js'></script>
+        <script type='text/javascript' src='js/modernizr.js'></script>
+        <script type='text/javascript' src='js/response.js'></script>
+        <script type='text/javascript' src='js/swiper.js'></script>
+        <script type='text/javascript' src='js/waypoints.js'></script>
+        <script type='text/javascript' src='js/jquery.stellar.js'></script>
+        <script type='text/javascript' src='js/module.js'></script>
+        <script type='text/javascript' src='js/bootstrap.min.js'></script>
+        <script src="js/wow.min.js"></script>
+        <script>
+            new WOW().init();
+
+        </script>
+
+    </body>
+
+    </html>
---
+++ php/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+
+
+    <meta charset="UTF-8" />
+    <meta name="description" content="" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> Home Maison Chic </title>
+    <!-- Windows 8 Tiles -->
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <!-- ****** faviconit.com favicons ****** -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css" />
+    <link rel="stylesheet" href="css/picto-foundry-emotions.css" />
+    <link rel="stylesheet" href="css/picto-foundry-household.css" />
+    <link rel="stylesheet" href="css/picto-foundry-shopping-finance.css" />
+    <link rel="stylesheet" href="css/picto-foundry-general.css" />
+    <link rel="stylesheet" href="css/booknoivas.css" />
+    <link rel="stylesheet" href="css/w3.css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="shortcut icon" href="images/m.png" />
+
+    <!-- Aqui vai as Tags das Redes Sociais, é o que faz a miniatura no facebook!-->
+    <!-- Motores de Busca -->
+    <meta name="description" content="Compartilhe elegância e a beleza que você merece, tudo isso e muito mais você encontra em primeira mão no Maison Chic.">
+    <meta name="image" content="https://imgur.com/a/PzGht">
+    <!-- Schema.org for Google -->
+    <meta itemprop="name" content="Maison Chic">
+    <meta itemprop="description" content="Compartilhe a elegância e beleza que você merece, tudo isso e nós oferecemos em primeira mão para no Maison Chic.">
+    <meta itemprop="image" content="https://imgur.com/a/PzGht">
+    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+    <meta name="og:title" content="Maison Chic Coiffeur">
+    <meta name="og:description" content="Compartilhe a elegância e beleza que você merece, tudo isso e nós oferecemos em primeira mão para no Maison Chic.">
+    <meta name="og:image" content="https://imgur.com/a/k1E1O">
+    <meta name="og:url" content="http://www.maizon.juracer.xyz">
+    <meta name="og:site_name" content="Maison Chic Coiffeur">
+    <meta name="og:locale" content="pt_BR">
+    <meta name="fb:app_id" content="1168589039940856">
+    <meta name="og:type" content="website">
+    <!-- Fim das Metas Tags -->
+
+    <!-- Identificação do APP e da URL -->
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10&appId=1168589039940856";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+    </script>
+
+
+
+
+</head>
